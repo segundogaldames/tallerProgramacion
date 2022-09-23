cd57cbe07a0344c93307f6ca03f4e11ff3f55f23
Segundo Galdames <segundogaldames@gmail.com>
2022-09-23 10:55:20 -0300

    Proyecto de Taller de Programacion Colaboracion de objetos

---
+++ ColaboracionClases/Banco.cs
@@ -0,0 +1,43 @@
+﻿using ColaboracionClases;
+using System;
+
+
+    class Banco
+    {
+        //declaramos 3 atriibutos de tipo Cliente (de la clase Cliente)
+        private Cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+        {
+            //instanciamos los atrbutos en objetos de la clase cliente
+            this.cliente1 = new Cliente("Juan");
+            this.cliente2 = new Cliente("Maria");
+            this.cliente3 = new Cliente("Raul");
+        }
+
+        public void Operar()
+        {
+            cliente1.Depositar(300000);
+            cliente2.Depositar(200000);
+            cliente3.Depositar(50000);
+            cliente3.Girar(40000);
+        }
+
+        public void Informacion()
+        {
+            int valores = cliente1.RetornarMonto() + cliente2.RetornarMonto() + cliente3.RetornarMonto();
+            
+            Console.WriteLine("Total depositos bancarios: " +valores);
+            cliente1.Imprimir();
+            cliente2.Imprimir();
+            cliente3.Imprimir();
+        }
+
+        static void Main(string[] args)
+        {
+            Banco banco = new Banco();
+            banco.Operar();
+            banco.Informacion();
+        }
+    }
+
---
+++ ColaboracionClases/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ColaboracionClases
+{
+    class Cliente
+    {
+        private string nombre;
+        private int monto;
+
+        public Cliente(string nombre)
+        {
+            this.nombre = nombre;
+            this.monto = 0;
+        }
+
+        public void Depositar(int valor)
+        {
+            this.monto = this.monto + valor;
+        }
+
+        public void Girar(int valor)
+        {
+            if (this.monto >= valor)
+            {
+                this.monto = this.monto - valor;
+            }
+            else
+            {
+                Console.WriteLine("No tiene fondos suficientes... intente depostar");
+            }
+        }
+
+        //metodo que retorna el monto de tipo entero
+        public int RetornarMonto() 
+        { 
+            return this.monto; 
+        }
+        //metodo que retorna el nombre del cliente de tipo string
+        public void Imprimir() 
+        {
+            Console.WriteLine(this.nombre + " Tiene depositado " + this.monto);
+        }   
+    }
+}
